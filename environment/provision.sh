34830ce65f6970c618e6ce29e63b01a59aed8078
Mohammad Usman Khan <mohammeduk@spartaglobal.co>
2017-08-08 11:05:57 +0100

    Add all files for ruby task

---
+++ environment/provision.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install nginx
+sudo apt-get update -y
+sudo apt-get install nginx -y
+
+# Update and Upgrade
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update -y
+sudo apt-get install ruby2.4 ruby2.4-dev -y
+
+# Install Gems
+sudo apt-get install build-essential zlib1g-dev libgmp-dev libxml2 -y
+sudo gem install bundler
+sudo gem install rails
+sudo gem install rake
